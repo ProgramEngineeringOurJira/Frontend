8685d64ff263f586ea147ad4a51e730eaf941aa2
Kristina <kasyankn@gmail.com>
2023-11-18 14:17:00 +0300

    boardSlice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import columnsSlice from './features/columns/columnsSlice';
+import boardSlice from './features/boardSlice';
 
 export const store = configureStore({
   reducer: {
     columns: columnsSlice.reducer,
+    board: boardSlice
   }
 });
