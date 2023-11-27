c78e7ea6688acfd6139ff1d3a8cb511749d6c01c
Mayorov Alexander <mayorov.shurik@mail.ru>
2023-11-27 16:53:24 +0300

    add comments modal adding - post request

--- src/components/AddCommentModal/AddCommentModal.tsx
+++ src/components/AddCommentModal/AddCommentModal.tsx
@@ -1,6 +1,7 @@
-import { FC, useState } from 'react';
-import { Form, Formik, FormikHelpers } from 'formik';
+import { FC, FormEvent, useState } from 'react';
+import { useParams } from 'react-router';
 
+import { useSendRequest } from '../../hooks/useSendRequest';
 import { TextForm } from '../../ui-kit/TextForm';
 import { FormElementWrapper } from '../../ui-kit/FormElementWrapper';
 import { Textarea } from '../../ui-kit/Textarea';
@@ -8,27 +9,40 @@ import { Button } from '../../ui-kit/Button';
 
 import styles from './styles.module.scss';
 
-interface Values {
-  text: string;
-}
-
 type AddCommentModalProps = {
   hide: () => void;
 };
 
 export const AddCommentModal: FC<AddCommentModalProps> = ({ hide }) => {
+  const [validationError, setValidationError] = useState('');
   const [text, setText] = useState('');
+  const { idBoard, idIssue } = useParams();
+
+  const postComment = (data: any) => {
+    console.log(data);
+    hide();
+  };
+
+  const { sendRequest, isError } = useSendRequest(postComment, `${idBoard}/issues/${idIssue}/comments`);
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const commentData = {
+      text: text
+    };
+
+    if (text.length > 0) {
+      sendRequest(commentData);
+      setValidationError('');
+      setText('');
+    } else {
+      setValidationError('Comment text must be non-empty');
+    }
+  };
 
   return (
-    <Formik
-      initialValues={{
-        text: ''
-      }}
-      onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
-        hide();
-      }}
-    >
-      <Form>
+    <>
+      <form onSubmit={onSubmit}>
         <div className={styles.AddCommentModal}>
           <FormElementWrapper>
             <TextForm text="Comment" />
@@ -38,7 +52,9 @@ export const AddCommentModal: FC<AddCommentModalProps> = ({ hide }) => {
         <div className={styles['AddCommentModal__button-submit']}>
           <Button text="Add Comment" type="primary" />
         </div>
-      </Form>
-    </Formik>
+      </form>
+      {isError && <span className={styles.error}>Invalid data format</span>}
+      {validationError && <span className={styles.error}>{validationError}</span>}
+    </>
   );
 };
