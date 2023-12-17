9a8a52b24a582b22cd2aa1090a4cda4c8791c556
Artemy Gladkov <gladkov.art76@gmail.com>
2023-12-17 12:36:23 +0300

    fix home button

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -1,3 +1,4 @@
+import {useParams } from 'react-router';
 import { FC, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -6,7 +7,6 @@ import { AddSprintModal } from '../../AddSprintModal';
 import { Modal, useModal } from '../../Modal';
 import { AuthContext } from '../../../shared/context';
 import { paths } from '../../../utils/paths';
-
 import styles from './styles.module.scss';
 
 type NavigationProps = {
@@ -14,6 +14,7 @@ type NavigationProps = {
 };
 
 export const Navigation: FC<NavigationProps> = ({ isCreateSprintVisible = true }) => {
+  const {idBoard, idSprint} = useParams();
   const { isShown, toggle } = useModal();
   const { clearAll, user } = useContext(AuthContext);
   const navigate = useNavigate();
@@ -24,7 +25,12 @@ export const Navigation: FC<NavigationProps> = ({ isCreateSprintVisible = true }
   };
 
   const onKristiClick = () => {
-    navigate(paths.home);
+    if(idBoard && idSprint)
+      navigate(`${paths.board}/${idBoard}${paths.sprint}/${idSprint}`);
+    else if(idBoard)
+      navigate(`${paths.board}/${idBoard}`);
+    else
+      navigate(paths.home);
   };
 
   return (
